17970b01fdd0119053f7732c70e518dbee45c34d
pattapon-son <pattapon.son@student.mahidol.edu>
2025-03-28 01:05:58 +0700

    Removed unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Space_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Space_Mono } from "next/font/google";
+import "./globals.css"; 
 
 const spaceMono = Space_Mono({
   variable: "--font-space-mono",
